67020e04c0ed0c3c2115364ac316bc809b8b0794
aasthapandey19 <asthaprod99@gmail.com>
2024-07-31 02:55:35 +0530

    fresh deck initialized

---
+++ cards/deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// new type to extend string data typee
+type deck []string
+
+func newDeck() deck {
+	freshCards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Clubs", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			createdCard := value + " of " + suite
+			freshCards = append(freshCards, createdCard)
+			fmt.Println(createdCard)
+		}
+	}
+	return freshCards
+}
+
+func (d deck) print() {
+	for i, x := range d {
+		fmt.Println(i, x)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,8 @@
 package main
 
-import "fmt"
+//import "fmt"
 
 func main() {
-	card := []string{"Ace of Diamonds", newCard()}
-	card = append(card, "Joker")
-	for i, car := range card {
-		fmt.Println(i, car)
-	}
-}
-
-func newCard() string {
-	return "five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
