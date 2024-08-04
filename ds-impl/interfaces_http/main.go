d960f82e472ad5ef39c0123a1f6fadc8727e9f9b
aasthapandey19 <asthaprod99@gmail.com>
2024-08-05 04:00:15 +0530

    implementing custom writer interface

--- ds-impl/interfaces_http/main.go
+++ ds-impl/interfaces_http/main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error is :", err)
 		os.Exit(1)
 	}
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
 
-	io.Copy(os.Stdout, resp.Body)
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
+
 }
